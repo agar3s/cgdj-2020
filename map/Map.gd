2c72c2e39b4de002917812a64b4e31802403c16c
Agar3s <ekeisco@gmail.com>
2020-03-30 20:42:33 -0500

    game of life rules implemented

--- map/Map.gd
+++ map/Map.gd
@@ -2,16 +2,37 @@ extends Node2D
 
 var Tile = preload("res://map/Tile.tscn")
 
+export (int) var tile_size = 13
+export (int) var radius = 13
+
+export (int) var min_survive = 2 
+export (int) var max_survive = 5
+
+export (int) var min_born = 3
+export (int) var max_born = 3
+
+var map: Dictionary = {}
+var playing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 
 	# sort layout
 	var x = -5.0
 	var y = -5.0
 	var z = 0.0
 	var index = 0
-	var radius = 3
+	
+	$Play.connect("button_down", self, "toggle_play")
+	$Timer.connect("timeout", self, "next_step")
+	$Next.connect("button_down", self, "next_step")
+	$Random.connect("button_down", self, "random_populate")
+	$Clear.connect("button_down", self, "clear")
+	$MinSurvive.value = min_survive
+	$MaxSurvive.value = max_survive
+	$MinBorn.value = min_born
+	$MaxBorn.value = max_born
 
 	for i in range(-radius, radius+1):
 		var r1 = max(-radius, -i - radius)
@@ -19,8 +40,66 @@ func _ready():
 		for z in range(r1, r2+1):
 			y = -i - z
 			var tile = Tile.instance()
-			tile.size = 35
+			tile.size = tile_size
 			tile.coordinates = Vector3(i, y, z)
+			tile.connect("mouse_exited", self, "on_tile_exit", [tile])
+			tile.connect("mouse_entered", self, "on_tile_hover", [tile])
 			$Tiles.add_child(tile)
+			map[tile.get_hash()] = tile
 
+	# clean neighbors
+	for tile in $Tiles.get_children():
+		var subset = []
+		for neighbor_hash in tile.neighbors:
+			if map.has(neighbor_hash):
+				subset.append(neighbor_hash)
+		tile.neighbors = subset
+
+	random_populate()
 	print($Tiles.get_child_count())
+
+
+func on_tile_hover(tile):
+	for neighbor_hash in tile.neighbors:
+		map[neighbor_hash].set_neighbor_hover(true)
+
+
+func on_tile_exit(tile):
+	for neighbor_hash in tile.neighbors:
+		map[neighbor_hash].set_neighbor_hover(false)
+	
+
+func next_step():
+	min_survive = $MinSurvive.value
+	max_survive = $MaxSurvive.value
+	min_born = $MinBorn.value
+	max_born = $MaxBorn.value
+	
+	for tile in $Tiles.get_children():
+		tile.alive_neighbors = 0
+		for neighbor_hash in tile.neighbors:
+			tile.alive_neighbors += map[neighbor_hash].live_factor if map[neighbor_hash].selected else 0
+
+	for tile in $Tiles.get_children():
+		tile.solve_next(min_survive, max_survive, min_born, max_born)
+
+
+func random_populate():
+	for tile in $Tiles.get_children():
+		tile.selected = randf() > $RandomBox.value
+		tile.live_factor = 1.0
+
+func clear():
+	for tile in $Tiles.get_children():
+		tile.selected = false
+		tile.live_factor = 1.0
+
+func toggle_play():
+	playing = !playing
+	if playing:
+		$Timer.start()
+		$Play.text = 'Pause'
+	else:
+		$Timer.stop()
+		$Play.text = 'Play'
+
--- map/Tile.gd
+++ map/Tile.gd
@@ -1,17 +1,36 @@
 extends Node2D
 
+signal mouse_entered
+signal mouse_exited
+
 export (int) var size = 100 setget set_size
 export (float) var stroke = 0.05
 
+
+export (Color) var outline_color = Color(0.99, 0.22, 0.2)
+export (Color) var outline_hover_color = Color(0.11, 0.95, 1.00)
+export (Color) var base_color = Color(0.03, 0.03, 0.09)
+export (Color) var selected_color = Color(0.8, 0.76, 0.8)
+
+var current_color = base_color
+
 var coordinates = Vector3(0.0, 0.0, 0.0) setget set_coordinates
 
-var selected = false
+var selected = false setget set_selected
+var neighbor_hover = false setget set_neighbor_hover
+
+var neighbors = []
+var alive_neighbors = 0
+
+var live_factor = 1.0
 
 func _ready():
 	set_size(size)
 	$Area2D.connect("mouse_entered", self, "on_hover")
 	$Area2D.connect("mouse_exited", self, "on_exit")
 	$Area2D.connect("input_event", self, "on_input")
+	$Back.color = outline_color
+	$Shape.color = base_color
 
 func set_size(value):
 	size = value
@@ -36,19 +55,66 @@ func set_coordinates(value):
 	self.position.x = (sqrt(3.0)*coordinates.x + coordinates.z*sqrt(3.0)/2.0)*size*(1.0+stroke)
 	self.position.y = (1.5 * coordinates.z)*size*(1.0+stroke)
 	$Label.text = str(coordinates.x) + '.' + str(coordinates.y) + '.' + str(coordinates.z)
+	set_neighbor_coordinates()
+
+func set_neighbor_coordinates():
+	neighbors = [
+		hash(Vector3(coordinates.x + 1, coordinates.y + 0, coordinates.z - 1)),
+		hash(Vector3(coordinates.x + 1, coordinates.y - 1, coordinates.z + 0)),
+		hash(Vector3(coordinates.x + 0, coordinates.y - 1, coordinates.z + 1)),
+		hash(Vector3(coordinates.x - 1, coordinates.y + 0, coordinates.z + 1)),
+		hash(Vector3(coordinates.x - 1, coordinates.y + 1, coordinates.z + 0)),
+		hash(Vector3(coordinates.x + 0, coordinates.y + 1, coordinates.z - 1))
+	]
+
+func get_hash():
+	return hash(coordinates)
 
 func on_hover():
-	$Back.color.a = 1.0
+	$Back.color = outline_hover_color
 	z_index = 25
+	yield(get_tree().create_timer(0.01), 'timeout')
+	emit_signal("mouse_entered")
 
 func on_exit():
-	$Back.color.a = 0.0
+	$Back.color = outline_color
 	z_index = 0
+	emit_signal("mouse_exited")
 
-func on_input(node, event, shape):
-	if event is InputEventMouseButton and event.pressed:
-		if selected:
-			$Shape.color = Color(0.91, 0.92, 0.52)
-		else:
-			$Shape.color = Color(0.2, 0.8, 0.7)
-		selected = !selected
+func set_neighbor_hover(value):
+	neighbor_hover = value
+	if neighbor_hover:
+		$Shape.color += Color(0.1, 0.1, 0.1)
+	else:
+		$Shape.color = current_color
+
+	
+func set_selected(value):
+	selected = value
+	if selected:
+		current_color = selected_color
+	else:
+		current_color = base_color
+	$Shape.color = current_color
+
+
+func on_input(_node, _event, _shape):
+	if _event is InputEventMouseButton and _event.pressed:
+		self.selected = !selected
+
+
+func solve_next(min_survive, max_survive, min_born, max_born):
+	if selected:
+		self.selected =  alive_neighbors >= min_survive && alive_neighbors <= max_survive
+		#if alive_neighbors == min_survive:
+		#	live_factor += 0.1
+		#elif alive_neighbors == max_survive:
+		#	live_factor -= 0.1
+	elif alive_neighbors >= min_born && alive_neighbors <= max_born:
+		self.selected = true
+		live_factor = 1.0
+	
+	$Label.text = str(live_factor)
+
+
+
